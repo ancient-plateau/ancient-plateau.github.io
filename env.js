3ffc2c85b544d31ba81dcd86fde59b369db96b13
ancient-plateau <ancientplateau@fastmail.fm>
2020-02-05 20:11:22 +1100

    initial

---
+++ env.js
@@ -0,0 +1,8 @@
+window.env = {"cognito-config": {"region": "ap-southeast-2",
+                                 "userPoolId": "ap-southeast-2_RSIExYeMZ",
+                                 "userPoolWebClientId": "7o2cieoplj0cjsjb7tt83d69hq",
+                                 "cookieStorage": {"domain": "ancient-plateau.github.io",
+                                                   "secure": true,
+                                                   "path":   "/",
+                                                   "expiry": 30}},
+              "api-base-url": "https://ancient-plateau.herokuapp.com"};
